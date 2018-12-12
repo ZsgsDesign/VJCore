4a01b7d1f993e65b9b90a065b11bd52378a6e9ab
ZsgsDesign <zsgsdesign@gmail.com>
2018-12-12 22:51:19 +0800

    GBK charset

--- 500A.cpp
+++ 500A.cpp
@@ -3,17 +3,17 @@ int main()
 {
     int n,m,a[30010];
     scanf("%d%d", &n, &m);
-	int sum=1,flag=0;
+	int sum=1,flag=0;//flag标记是否能到达t那个房子 ，sum初始化为1 
     for(int i=1; i<n; i++)
     {
-   		scanf("%d",&a[i]);
+   		scanf("%d",&a[i]);//输入第i个房子能往后到达的指定距离 
 	}
 	int i=1;
 	while(i<n)
 	{
 		sum=sum+a[i];
     	i=sum;
-        if(i==m)
+        if(i==m)//一旦到达了这个房子，那么标记一下，跳出 
 		{
             flag = 1;
             break;
